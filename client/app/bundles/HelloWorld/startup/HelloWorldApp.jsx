879dd4853ba90647dc3616cfb4b789bb5a3a027d
Justin Gordon <justin.gordon@gmail.com>
2016-11-18 19:34:07 -1000

    Ran rails generate react_on_rails:install
    Using v6

---
+++ client/app/bundles/HelloWorld/startup/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactOnRails from 'react-on-rails';
+
+import HelloWorldContainer from '../containers/HelloWorldContainer';
+
+const HelloWorldApp = (props) => (
+  <HelloWorldContainer {...props} />
+);
+
+// This is how react_on_rails can see the HelloWorldApp in the browser.
+ReactOnRails.register({ HelloWorldApp });
