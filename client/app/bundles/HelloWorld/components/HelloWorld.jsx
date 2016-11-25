9f2769ded6dff7679d26e544d7a1b06baec8944c
Justin Gordon <justin.gordon@gmail.com>
2016-11-24 23:53:52 -1000

    one prop per line

--- client/app/bundles/HelloWorld/components/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -33,7 +33,9 @@ export default class HelloWorld extends React.Component {
             Say hello to:
           </label>
           <input
-            type="text" value={this.state.name} id="name"
+            type="text"
+            value={this.state.name}
+            id="name"
             onChange={(e) => this.updateName(e.target.value)}
           />
         </form>
